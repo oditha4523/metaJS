094e5304f3e4b01e193decb311434ca38e2a5d48
oditha4523 <odithachamod@gmail.com>
2025-02-28 19:39:43 +0530

    initial commit

---
+++ math_obj.js
@@ -0,0 +1,20 @@
+// var clothingItem = {
+//   price: 50,
+//   color: 'beige',
+//   material: 'cotton',
+//   season: 'autumn'
+// }
+
+// for(var key of Object.keys(clothingItem)){
+//   console.log(key, ':', clothingItem[key]);
+  
+// }
+
+const poem = `Roses are red
+Violets are blue
+JavaScript is fun
+And so are you.`;
+
+console.log(poem);
+
+
