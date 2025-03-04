d6441edd633c8670f13e4e8e8df0b4abfe37c13b
oditha4523 <odithachamod@gmail.com>
2025-03-04 22:29:35 +0530

    new add

---
+++ forOfLoop.js
@@ -0,0 +1,18 @@
+const fruits=["apple","orange","mango","papaya"];
+
+function appendix(fruit,index){
+    console.log(`${index} : ${fruit}`);
+}
+
+fruits.forEach(appendix);
+
+const result = [];
+const drone = {
+    speed: 100,
+    color: 'yellow'
+}
+const droneKeys = Object.keys(drone);
+droneKeys.forEach( function(key) {
+    result.push(key, drone[key])
+})
+console.log(result)
